29567fd7df74000604acb39570a69591c60d1b02
Rajudv19 <rajudv1997@gmail.com>
2022-06-09 09:17:10 +0530

    Added : Code to Createa DB Table

---
+++ TableCreation.java
@@ -0,0 +1,42 @@
+package com.BridgeLabs.day34;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TableCreation {
+	public static void main(String[] args)  {
+		String query = "CREATE TABLE employee_payroll " +
+				"(column_id INTEGER not NULL, " +
+				" first VARCHAR(255), " + 
+				" last VARCHAR(255), " + 
+				" salary INTEGER, " + 
+				" PRIMARY KEY ( column_id ))"; 
+
+		Connection connection = null;
+		Statement statement = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver class loaded and registered");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeepayroll_service? user=root & password=raju");
+			System.out.println("Connection established with db server");
+			statement = connection.createStatement();
+			System.out.println("platform created");
+			statement.executeQuery(query);
+			System.out.println("Data selected form DB");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		finally{
+			if(statement!=null){
+				try{
+					connection.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
